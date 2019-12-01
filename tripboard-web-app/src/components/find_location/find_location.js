08154fbe353f356e4bf7817b53485d66b30d1b98
Tariq Patanam <t.ahmed.p@gmail.com>
2019-12-01 02:33:47 -0600

    [App] Searh bar writes to state.

--- tripboard-web-app/src/components/find_location/find_location.js
+++ tripboard-web-app/src/components/find_location/find_location.js
@@ -4,16 +4,34 @@ import {Col, Container, FormControl, Row} from 'react-bootstrap';
 
 import React from 'react';
 
-function FindLocation() {
-  return (
-    <Container fluid={true}>
-      <Row>
-        <Col>
-          <FormControl className="searchBar" size="large" type="text" placeholder="Search..."/>
-        </Col>
-      </Row>
-    </Container>
-  );
+class FindLocation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchInput: '',
+    };
+  }
+
+  onSearchInputChange(searchInput){
+    this.setState({searchInput});
+  }
+
+  render() {
+    return (
+      <Container fluid={true}>
+        <Row>
+          <Col>
+            <FormControl
+              className="searchBar"
+              onChange={(event) => this.onSearchInputChange(event.target.value)}
+              placeholder="Where to?"
+              size="large"
+              type="text"/>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default FindLocation;
